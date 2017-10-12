b4cd1f86d9f99adbe5266d3bfabe1d043b2c9f9c
sebastianWV <stickert@worldviz.com>
2017-10-12 17:07:56 +0200

    initial commit

---
+++ db_test.py
@@ -0,0 +1,48 @@
+﻿import db.base
+from db.api import Api
+from db.models import tables
+
+def set_database_to_default(recreate=True):
+    a = None
+    if recreate:
+        db.base.recreate_database()
+        a = Api(bind=db.base.engine)
+    else:
+        a = Api(bind=db.base.engine)
+        a._clear()
+    a.close()
+    del a
+    a = None
+
+if __name__ == "__main__":
+	import random
+	set_database_to_default(False)
+	a = Api(bind=db.base.engine)
+	a.open()
+
+	rand_name = "foo"+str(random.randint(0,100000))+".mp3"
+
+	s = tables.SoundFile()
+	s.name = rand_name
+	s.path = "path/to/" + rand_name
+	s.relative_path = "to/" + rand_name
+	
+	c = tables.Category()
+	c.name = "my first cat"
+
+	sf_c = tables.SoundFileCategory()
+	sf_c.sound_file = s
+	sf_c.category = c
+	
+	a.insert_all([s,c,sf_c])
+	
+	if not a.commit():
+		print "Failure: Could not add SoundFile."
+	
+	res = a.query(tables.SoundFileCategory)
+	print res
+	for row in res:
+		print row.sound_file
+		
+	a.close()
+	set_database_to_default()
---
+++ setup.py
@@ -0,0 +1,30 @@
+﻿import virtualenv
+import pip
+import os
+import platform
+
+# create virtual environment
+print(" > creating virtualenv")
+env_dir = os.path.dirname(os.path.realpath(__file__)) + '\\env'
+print("   > path:" + env_dir)
+virtualenv.create_environment(env_dir, clear=True)
+
+# activate virtual environment
+print(" > activating virtualenv from activation script")
+activate_file = os.path.join(env_dir, "Scripts", "activate_this.py")
+if platform.system() != 'Windows':
+    activate_file = os.path.join(env_dir, "bin", "activate_this.py")
+print("   > path:" + activate_file)
+execfile(activate_file)
+
+# install requirements from pip
+print(" > installing pip requirements from file")
+requirements_file = os.path.dirname(os.path.realpath(__file__)) + "\\requirements.txt"
+print("   > path:" + requirements_file)
+pip.main(["install", "--ignore-installed", "--prefix", env_dir, "-r", requirements_file])
+
+# create database
+import db.base
+from db.models import tables
+print(" > creating database")
+db.base.recreate_database()
