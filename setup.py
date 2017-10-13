3436b8280f0b2c5709aaf6c39e5463c3af611a14
EphTron <es.90@gmx.de>
2017-10-13 14:58:08 +0200

    changed --prefix install to --target

--- setup.py
+++ setup.py
@@ -2,6 +2,7 @@
 import pip
 import os
 import platform
+import sys
 
 # create virtual environment
 print(" > creating virtualenv")
@@ -15,13 +16,16 @@ activate_file = os.path.join(env_dir, "Scripts", "activate_this.py")
 if platform.system() != 'Windows':
     activate_file = os.path.join(env_dir, "bin", "activate_this.py")
 print("   > path:" + activate_file)
-execfile(activate_file)
+if sys.version_info[0] < 3:
+	execfile(activate_file)
+else:
+	exec(open(activate_file).read())
 
 # install requirements from pip
 print(" > installing pip requirements from file")
 requirements_file = os.path.dirname(os.path.realpath(__file__)) + "\\requirements.txt"
 print("   > path:" + requirements_file)
-pip.main(["install", "--ignore-installed", "--prefix", env_dir, "-r", requirements_file])
+pip.main(["install", "--ignore-installed", "--target", env_dir, "-r", requirements_file])
 
 # create database
 import database.base
