25808ca584f2ae4d25275986c941e6ae1c38a2b2
sebastianWV <stickert@worldviz.com>
2017-11-17 01:52:22 +0100

    update and test submodule

--- db_test.py
+++ db_test.py
@@ -1,48 +1,47 @@
-﻿import db.base
-from db.api import Api
-from db.models import tables
+﻿import database.base
+from database.api import Api
+from database.models import tables
 
 def set_database_to_default(recreate=True):
-    a = None
-    if recreate:
-        db.base.recreate_database()
-        a = Api(bind=db.base.engine)
-    else:
-        a = Api(bind=db.base.engine)
-        a._clear()
-    a.close()
-    del a
-    a = None
-
+  a = None
+  if recreate:
+    database.base.recreate_database()
+    a = Api(bind=database.base.engine)
+  else:
+    a = Api(bind=database.base.engine)
+    a._clear()
+  a.close()
+  del a
+  a = None
+  
 if __name__ == "__main__":
-	import random
-	set_database_to_default(False)
-	a = Api(bind=db.base.engine)
-	a.open()
-
-	rand_name = "foo"+str(random.randint(0,100000))+".mp3"
-
-	s = tables.SoundFile()
-	s.name = rand_name
-	s.path = "path/to/" + rand_name
-	s.relative_path = "to/" + rand_name
-	
-	c = tables.Category()
-	c.name = "my first cat"
-
-	sf_c = tables.SoundFileCategory()
-	sf_c.sound_file = s
-	sf_c.category = c
-	
-	a.insert_all([s,c,sf_c])
-	
-	if not a.commit():
-		print "Failure: Could not add SoundFile."
-	
-	res = a.query(tables.SoundFileCategory)
-	print res
-	for row in res:
-		print row.sound_file
-		
-	a.close()
-	set_database_to_default()
+  a = Api(bind=database.base.engine)
+  a.open()
+  s = a._db
+  
+  p = tables.Preset(name='empty nested')
+  p.json = """{
+  "data": {
+      "contents": {
+          "scene": {
+              "scene_rect": {
+                  "height": 1258,
+                  "width": 2221,
+                  "x": 0,
+                  "y": 0
+              },
+              "tiles": []
+          }
+      },
+      "name": "Nature",
+      "position": [
+          1074,
+          22
+      ],
+      "size": 2,
+      "uuid": "{b712a3cf-ca9b-40b7-9925-39377a758fc4}"
+  },
+  "type": "TwoD::NestedTile"
+  }"""
+  s.add(p)
+  s.commit()
